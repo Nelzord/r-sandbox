6f08ed7854ae19694c7d43717a2ac5e84d5e1696
Jack Nelson <jmn3lson@gmail.com>
2021-01-12 19:08:49 -0800

    weibul update

--- main.r
+++ main.r
@@ -1,7 +1,25 @@
-f <- function(x) (90*x^8)*(1-x)
+#WEIBUL#
+install.packages("WeibullR")
+library(WeibullR)
 
-plot(f, xlim = c(0,1), ylim = c(0,4), main = "sandbox")
+install.packages("stats")
+library(stats)
 
-area <- integrate(f, lower = 0, upper = 1)
-area
+curve(dweibull(x, shape=9, scale=180), from=0, to=350, main = "Section 1.5 Exercise 45, Weibull Density, f(x)", xlab = "x = Tensile Strength in MPa", ylab = "y = Probability")
+#proportion of values that have strength exceeding 90
+pweibull(90, 9, 180, lower.tail = FALSE)
+
+#proportion of values between 150 and 175
+
+C <- pweibull(175, 9, 180, lower.tail = TRUE)
+c <- pweibull(150, 9, 180, lower.tail = TRUE)
+Ans45 <- C - c
+Ans45
+
+
+
+#strength value that seperates the weakest 10% from the strongest 90%
+qweibull(.10, 9, 180, lower.tail = TRUE)
+
+
 
